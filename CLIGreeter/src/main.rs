32356ea5c159d87aafd47d867f9c46719169c691
DylanGates <41268880+DylanGates@users.noreply.github.com>
2025-11-25 22:47:03 +0000

    added age

--- CLIGreeter/src/main.rs
+++ CLIGreeter/src/main.rs
@@ -3,8 +3,8 @@ use std::io;
 fn main() {
     println!(
         "
-    Nice to meet you!
-    What is your name?"
+Nice to meet you!
+What is your name?"
     );
     let mut name = String::new();
     io::stdin()
@@ -12,5 +12,13 @@ fn main() {
         .expect("Failed to read line");
     name = name.trim().to_string();
 
-    println!("Hello, {}", name);
+    println!("\nHello, {}\n", name);
+
+    println!("What is your age?");
+
+    let mut age = String::new();
+    io::stdin()
+        .read_line(&mut age)
+        .expect("Failed to read line");
+    print!("\nYou are {} years old!", age);
 }
