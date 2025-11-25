c95ef7198dbdc3a4abc9d39b0fb611d3d0cf4842
DylanGates <41268880+DylanGates@users.noreply.github.com>
2025-11-25 23:04:01 +0000

    added manner

--- CLIGreeter/src/main.rs
+++ CLIGreeter/src/main.rs
@@ -1,5 +1,33 @@
 use std::io;
 
+enum Manner {
+    Polite,
+    Rude,
+}
+
+impl Manner {
+    fn greet(&self, name: &str) -> String {
+        match self {
+            Manner::Polite => format!("\nHello, nice to meet you {}!", name.to_uppercase()),
+            Manner::Rude => format!(
+                "\nOh, it's you {}...,
+---\nI guess we have to say hi.\n",
+                name
+            ),
+        }
+    }
+}
+
+fn check_greeting(name: &str) -> Manner {
+    let first_char = name.chars().next().unwrap_or('\0');
+
+    if first_char.is_alphabetic() && first_char.is_uppercase() {
+        Manner::Polite
+    } else {
+        Manner::Rude
+    }
+}
+
 fn main() {
     println!(
         "
@@ -12,7 +40,14 @@ What is your name?"
         .expect("Failed to read line");
     name = name.trim().to_string();
 
-    println!("\nHello, {}\n", name);
+    let manner = check_greeting(&name);
+
+    let display_greeting = match manner {
+        Manner::Polite => manner.greet(&name),
+        Manner::Rude => manner.greet(&name),
+    };
+
+    println!("{}", display_greeting);
 
     println!("What is your age?");
 
