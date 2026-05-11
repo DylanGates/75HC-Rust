266404c56426ff0568056e8f97dd20a050927c5d
DylanGates <solomonafoh@gmail.com>
2026-05-11 23:43:11 +0000

    Add configurable password length flag

--- password_manager/src/main.rs
+++ password_manager/src/main.rs
@@ -3,13 +3,17 @@ use rand::Rng;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct Args {}
+struct Args {
+    /// Length of the password
+    #[arg(short, long, default_value_t = 12)]
+    length: usize,
+}
 
 fn main() {
-    let _args = Args::parse();
+    let args = Args::parse();
     let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     let mut rng = rand::thread_rng();
-    let password: String = (0..12)
+    let password: String = (0..args.length)
         .map(|_| {
             let idx = rng.gen_range(0..charset.len());
             charset.chars().nth(idx).unwrap()
